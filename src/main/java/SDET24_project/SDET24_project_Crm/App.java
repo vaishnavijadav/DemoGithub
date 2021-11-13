2fd7931499713386b3ef601b97cc54cb45e6c668
vaishnavijadav <vaishnavijadav22@gmail.com>
2021-11-13 13:31:28 +0530

    framework

---
+++ src/main/java/SDET24_project/SDET24_project_Crm/App.java
@@ -0,0 +1,13 @@
+package SDET24_project.SDET24_project_Crm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/crm/Demo/Github/JenkinsTest/Demo1Test.java
@@ -0,0 +1,11 @@
+package crm.Demo.Github.JenkinsTest;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+	@Test
+	public void Demo() {
+		System.out.println("hello");
+	}
+
+}
